b9185dc49f46c15a51de755e0ea52f4da05bac98
Parikshith S <parikshith.s@siddhanintelligence.com>
2026-09-10 19:41:53 +0530

    chore: clear remote cache index completely

--- e2e/01.login.spec.ts
+++ e2e/01.login.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import { LoginPage } from '../pages/loginPage';
+import { LoginPage } from '../pages/LoginPage';
 
 test('orangeHrmLoginPage', async ({ page }) => {
     const loginPage = new LoginPage(page);
